ece0bbf45bdf2d5454c24fcfbb112556728f06c3
Mihira Jayasekera <mihira@tinymachine.net>
2022-07-08 14:27:31 -0700

    chore: set up for Svelte and update Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte'
 import yaml from '@rollup/plugin-yaml'
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [svelte()],
   vite: {
     plugins: [yaml()]
   }  

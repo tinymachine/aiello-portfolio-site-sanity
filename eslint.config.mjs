6b2174931c5d78805cb4f17a31b3fc29fb9f81af
Mihira Jayasekera <mihira@tinymachine.net>
2024-07-16 15:05:00 -0500

    chore: add eslint-config-prettier
    For eslint to work well with prettier (not sure exactly what this affects!)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import js from '@eslint/js'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import prettier from 'eslint-config-prettier'
 
 export default [
   js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  prettier,
   {
     rules: {
       // override/add rules settings here, such as:

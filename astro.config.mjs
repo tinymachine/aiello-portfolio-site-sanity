faaa963d7b6a97b6d52cf851febfd707321ee77c
Mihira Jayasekera <mihira@tinymachine.net>
2024-11-14 09:00:36 -0800

    wip: add Sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import yaml from '@rollup/plugin-yaml'
+import sanity from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    sanity({
+      projectId: 'oqzk4lvk',
+      dataset: 'production',
+      useCdn: false, // See note on using the CDN
+      apiVersion: '2024-07-24',
+    }),
+  ],
   vite: {
     plugins: [yaml()],
   },

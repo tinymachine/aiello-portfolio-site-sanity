d93f0a0399315c6b1610c5bf807901d34388fbd7
Mihira Jayasekera <mihira@tinymachine.net>
2025-01-21 23:33:22 -0800

    chore: remove unused Settings.md and siteInfo.js
    Relics of Forestry CMS and Cloudinary, which have been deprecated in favor of Sanity for both CMS and image CDN.

--- src/settings/siteInfo.js
+++
@@ -1,8 +0,0 @@
-import forestrySettings from '../../.forestry/settings.yml'
-const { public_path } = forestrySettings
-
-// Add Cloudinary image optimizations
-// ref: https://cloudinary.com/documentation/image_optimization#how_to_optimize_image_sizing
-const imagePath = public_path + `/f_auto,q_auto`
-
-export { imagePath }

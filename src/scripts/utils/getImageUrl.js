7f8a55a6f8628377947dd59e9a0b13a675b5ce1a
Mihira Jayasekera <mihira@tinymachine.net>
2025-01-26 21:40:59 -0800

    feat: add support for Open Graph share meta tags
    This correlates with an update to the Sanity Studio (CMS) config to specify an image for inclusion in the OG share tags, with the ability to specify a hotspot and a crop. (OG share meta tags allow you to customize how a URL preview appears when a link to the site is shared on social media or via a messaging app, including adding an image.)

---
+++ src/scripts/utils/getImageUrl.js
@@ -0,0 +1,44 @@
+export const getOgImageUrl = (
+  source,
+  { targetWidth = 1200, targetHeight = 630 }
+) => {
+  const getRectDims = ({ width, height, crop }) => {
+    // compute the cropped image's area
+    const croppedWidth = Math.floor(
+      width * (1 - ((crop?.right || 0) + (crop?.left || 0)))
+    )
+    const croppedHeight = Math.floor(
+      height * (1 - ((crop?.top || 0) + (crop?.bottom || 0)))
+    )
+
+    // compute the cropped image's position
+    const left = Math.floor(width * (crop?.left || 0))
+    const top = Math.floor(height * (crop?.top || 0))
+
+    return {
+      left,
+      top,
+      croppedWidth,
+      croppedHeight,
+    }
+  }
+
+  // Helper function to generate image URL with hotspot consideration
+  const urlFor = (source) => {
+    if (!source) return null
+
+    const { left, top, croppedWidth, croppedHeight } = getRectDims(source)
+    return `
+      ${source.url}
+      ?w=${targetWidth}
+      &h=${targetHeight}
+      &rect=${left},${top},${croppedWidth},${croppedHeight}
+      &focalpoint=${source.hotspot?.x || 0.5},${source.hotspot?.y || 0.5}
+      &fit=crop
+      &crop=focalpoint
+      &fm=jpg
+    `.replace(/\s/g, '')
+  }
+
+  return urlFor(source)
+}

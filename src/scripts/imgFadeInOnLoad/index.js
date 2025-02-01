0a54ca9b9cad7372ff9d0b0131d9ed28e9112364
Mihira Jayasekera <mihira@tinymachine.net>
2025-02-01 11:23:19 -0800

    docs(imgFadeInOnLoad): add explanatory comment

--- src/scripts/imgFadeInOnLoad/index.js
+++ src/scripts/imgFadeInOnLoad/index.js
@@ -1,3 +1,12 @@
+/**
+ * Fade in (and de-blur) images once they've loaded.
+ * Import the `fadeInImagesOnLoad()` script and execute it
+ * when the DOM has loaded (e.g. in the `onMount()` hook
+ * in Svelte components, or after the DOMContentLoaded
+ * event in Astro components). Apply it to <img> elements
+ * by adding the `data-fadein-onload` attribute.
+ */
+
 import './styles.css'
 
 export const fadeInImagesOnLoad = () => {

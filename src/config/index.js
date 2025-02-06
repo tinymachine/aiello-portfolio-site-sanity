63b5f44ac4efe6d3f9395e5d3df4d12af01255cb
Mihira Jayasekera <mihira@tinymachine.net>
2025-02-06 12:11:19 -0800

    perf(visual): improve default image quality
    Sanity's default is 75; increase to 90 (though larger filesizes).

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const IMG_GLOBAL_URL_PARAMS = '&fit=max&auto=format' // see https://www.sanity.io/docs/image-urls#BhPyF4m0
+export const IMG_GLOBAL_URL_PARAMS = '&q=90&fit=max&auto=format' // see https://www.sanity.io/docs/image-urls#BhPyF4m0
 export const MAX_DPR = 2 // maximum device pixel ratio (e.g. 2 for 2x density displays)
